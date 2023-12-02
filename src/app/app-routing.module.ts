c128d84c60565dc82dd349c9ddb30565342732cb
abdulla0207 <abdulla.ermatov0407@gmail.com>
2023-12-02 21:23:34 +0500

    Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import {NgModule} from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { GaugeComponent } from './gauge/gauge.component';
+
+const routes: Routes = [
+    {path: '', component: GaugeComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+export class AppRoutingModule{ }
